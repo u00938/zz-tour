e0bc639cb5426a486039463eaf17583631ddbe04
u00938 <yo01030184262@gmail.com>
2023-12-07 14:33:59 +0900

    feat: reservation user list, approval api

--- src/api/route/admin.ts
+++ src/api/route/admin.ts
@@ -3,6 +3,7 @@ import { Container } from 'typedi';
 import { celebrate, Joi } from 'celebrate';
 import { Logger } from 'winston';
 import AdminService from '@/service/admin';
+import middleware from '../middleware';
 
 const route = Router();
 
@@ -20,7 +21,7 @@ export default (app: Router) => {
       const logger: Logger = Container.get('logger');
 
       try {
-        const result = await AdminService.signIn(req.body);
+        const result = await AdminService.SignIn(req.body);
         return res.status(200).json(result);
       } catch (e) {
         logger.error('error %o', e);
@@ -28,5 +29,45 @@ export default (app: Router) => {
       }
     }
   );
+
+  route.get('/tour/reservation/user',
+    celebrate({
+      query: Joi.object({
+        date: Joi.string().required()
+      })
+    }),
+    middleware.isAuth, middleware.attachAdminUser,
+    async (req: Request, res: Response, next: NextFunction) => {
+      const logger: Logger = Container.get('logger');
+
+      try {
+        const result = await AdminService.GetReservationUser(req.query);
+        return res.status(200).json(result);
+      } catch (e) {
+        logger.error('error %o', e);
+        return next(e);
+      }
+    }
+  );  
+
+  route.put('/tour/reservation/approval',
+    celebrate({
+      body: Joi.object({
+        id: Joi.string().required()
+      })
+    }),
+    middleware.isAuth, middleware.attachAdminUser,
+    async (req: Request, res: Response, next: NextFunction) => {
+      const logger: Logger = Container.get('logger');
+
+      try {
+        const result = await AdminService.ApproveReservation(req.body);
+        return res.status(200).json(result);
+      } catch (e) {
+        logger.error('error %o', e);
+        return next(e);
+      }
+    }
+  );  
 
 }
--- src/service/admin.ts
+++ src/service/admin.ts
@@ -1,14 +1,18 @@
 import { AdminUser } from '@/model/entities/AdminUser';
+import { TourReservation } from '@/model/entities/TourReservation';
+import { User } from '@/model/entities/User';
+import { getBoolean } from '@/util/cast';
 import token from '@/util/token';
+import dayjs from 'dayjs';
 import { Service } from 'typedi';
-import { getManager } from 'typeorm';
+import { getConnection, getManager } from 'typeorm';
 
 @Service()
 export default class AdminService {
   constructor() {
   }
 
-  public static async signIn(userData): Promise<object> {
+  public static async SignIn(userData): Promise<object> {
     try {
       // 간단한 인증 용도로, 회원가입 로직이 생략되었기 때문에 비밀번호 암호화/해독 과정 또한 생략
       const user = await getManager('zz_tour').findOne(AdminUser, { 
@@ -29,6 +33,70 @@ export default class AdminService {
       const accessToken = await token.getJwtAccessToken(user);
 
       return { accessToken };
+    } catch (e) {
+      throw e;
+    }
+  }
+
+  public static async GetReservationUser(queryData): Promise<Array<object>> {
+    try {
+      const list = await getConnection('zz_tour')
+      .getRepository(TourReservation)
+      .createQueryBuilder('tourReservation')
+      .select([
+        'tourReservation.id AS tourReservationId',
+        'tourReservation.status_text AS statusText',
+        'user.id AS userId',
+        'user.username AS username',
+      ])
+      .innerJoin(User, 'user', 'user.id = tourReservation.user_id')
+      .where('tourReservation.tour_date = :tourDate', { tourDate: queryData.date })
+      .getRawMany();
+
+      return list;
+    } catch (e) {
+      throw e;
+    }
+  }  
+
+  public static async ApproveReservation(bodyData): Promise<string> {
+    try {
+      const history = await getConnection('zz_tour')
+      .getRepository(TourReservation)
+      .findOne({
+        where: {
+          id: bodyData.id
+        }
+      });
+
+      if (!history) {
+        const error = new Error(`유효하지 않은 내역`);
+        error.name = 'dev';
+        throw error;
+      }
+
+      if (history.cancel) {
+        const error = new Error(`이미 취소된 예약 건`);
+        error.name = 'dev';
+        throw error;
+      }
+
+      if (dayjs(history.tourDate).tz().isSameOrBefore(dayjs().tz().format('YYYY-MM-DD'))) {
+        const error = new Error(`예약 승인 불가한 날짜`);
+        error.name = 'dev';
+        throw error;
+      }
+
+      const updated = await getConnection('zz_tour')
+      .createQueryBuilder()
+      .update(TourReservation)
+      .set({
+        approval: 1
+      })
+      .where('id = :id', { id: bodyData.id })
+      .execute();
+
+      return `id - ${bodyData.id} 승인: ${getBoolean(updated.affected)}`;
     } catch (e) {
       throw e;
     }
